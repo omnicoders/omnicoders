157a5e885d23877b4acaf318269ba5cee086fdcd
Matthew Stewart <matthew.stewart.mi@gmail.com>
2018-06-02 11:28:18 -0700

    initial commit

---
+++ node/basics/README.md
@@ -0,0 +1,2 @@
+# Node Basics
+An introduction to Node JS through code examples.
---
+++ node/basics/console.js
@@ -0,0 +1,20 @@
+/* 
+  JavaScript Console
+  For more information:
+  https://developer.mozilla.org/en-US/docs/Web/API/console
+*/
+
+// clear the console
+console.clear();
+
+// start a timer
+console.time('Print Hello World To Console');
+
+// print two new lines and a message to the console
+console.log('\n\nHello World!');
+
+// end timer and print result to console
+console.timeEnd('Print Hello World To Console');
+
+// print two new lines
+console.log('\n\n');
